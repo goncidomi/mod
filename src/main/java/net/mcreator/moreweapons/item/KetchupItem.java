5aca2dd788aa3a9639d599b616dd2cbd65c2cd27
goncidomi <Gamer i5@DESKTOP-R5LU0R2>
2021-07-02 17:14:47 +0200

    ketchup jó

--- src/main/java/net/mcreator/moreweapons/item/KetchupItem.java
+++ src/main/java/net/mcreator/moreweapons/item/KetchupItem.java
@@ -1,27 +1,32 @@
 
 package net.mcreator.moreweapons.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.moreweapons.itemgroup.MoreWeaponsItemGroup;
+import net.mcreator.moreweapons.MoreWeaponsModElements;
+
 @MoreWeaponsModElements.ModElement.Tag
 public class KetchupItem extends MoreWeaponsModElements.ModElement {
-
-	@ObjectHolder("more_weapons_:ketchup")
+	@ObjectHolder("more_weapons_:ketchupcuc")
 	public static final Item block = null;
-
 	public KetchupItem(MoreWeaponsModElements instance) {
 		super(instance, 53);
-
 	}
 
 	@Override
 	public void initElements() {
 		elements.items.add(() -> new ItemCustom());
 	}
-
 	public static class ItemCustom extends Item {
-
 		public ItemCustom() {
 			super(new Item.Properties().group(MoreWeaponsItemGroup.tab).maxStackSize(64).rarity(Rarity.COMMON));
-			setRegistryName("ketchup");
+			setRegistryName("ketchupcuc");
 		}
 
 		@Override
@@ -38,7 +43,5 @@ public class KetchupItem extends MoreWeaponsModElements.ModElement {
 		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
 			return 1F;
 		}
-
 	}
-
 }
