0ca1e4e2a69e5769340c627bb13adbeb121806f0
M1neBa1int <user@DESKTOP-8K3H3JI>
2021-07-03 09:36:51 +0200

    tzututzuztu

--- src/main/java/net/mcreator/moreweapons/item/HeatpickaxeItem.java
+++ src/main/java/net/mcreator/moreweapons/item/HeatpickaxeItem.java
@@ -1,12 +1,20 @@
 
 package net.mcreator.moreweapons.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.moreweapons.MoreWeaponsModElements;
+
 @MoreWeaponsModElements.ModElement.Tag
 public class HeatpickaxeItem extends MoreWeaponsModElements.ModElement {
-
 	@ObjectHolder("more_weapons_:heatpickaxe")
 	public static final Item block = null;
-
 	public HeatpickaxeItem(MoreWeaponsModElements instance) {
 		super(instance, 63);
 	}
@@ -38,8 +46,6 @@ public class HeatpickaxeItem extends MoreWeaponsModElements.ModElement {
 				return Ingredient.EMPTY;
 			}
 		}, 1, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
-
 		}.setRegistryName("heatpickaxe"));
 	}
-
 }
